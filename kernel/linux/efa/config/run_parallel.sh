lneef/amzn-drivers

commit 4940b5a3e673c65dcf9ad934e0024913a067756d

linux/efa: Replace conftest PID wait with single parallel runner
Replace the runbg.sh + wait_for_pid.sh PID-polling machinery with
a single run_parallel.sh script that launches all conftests in
parallel and uses wait(1) on its own children. This eliminates the
nohup forking issue where $! could capture the wrong PID, causing
wait_for_pid to return early while conftests are still running.

The new flow accumulates conftest commands during CMake configure,
then executes them all at once via run_conftests.sh. Since all
processes are direct children of the same shell, wait(1) reliably
blocks until every conftest completes.

Also rename functions to reflect the new semantics:
  try_compile -> add_compile_conftest
  try_compile_dev_or_ops -> add_dev_op_compile_conftest
  try_compile_prog_test -> verify_kernel_headers

Reviewed-by: Yonatan Nachum <ynachum@amazon.com>
Signed-off-by: Michael Margolin <mrgolin@amazon.com>

kernel/linux/efa/config/run_parallel.sh
@@ -1,0 +1,11 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-2.0 OR BSD-2-Clause
+# Copyright 2026 Amazon.com, Inc. or its affiliates. All rights reserved.
+
+# Execute commands from a file in parallel and wait for all to complete.
+
+while IFS= read -r cmd; do
+	eval "$cmd" &
+done < "$1"
+
+wait

kernel/linux/efa/config/efa.cmake
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: GPL-2.0 OR BSD-2-Clause
-# Copyright 2021-2025 Amazon.com, Inc. or its affiliates. All rights reserved.
+# Copyright 2021-2026 Amazon.com, Inc. or its affiliates. All rights reserved.
 
 function(config_define def)
   file(APPEND ${CMAKE_CURRENT_BINARY_DIR}/config.h "#define ${def} 1\n")
@@ -13,7 +13,7 @@ function(set_conf_tmp_dir prologue body)
   configure_file(${CMAKE_SOURCE_DIR}/config/Makefile ${tmp_dir} COPYONLY)
 endfunction()
 
-function(try_compile_prog_test)
+function(verify_kernel_headers)
   set_conf_tmp_dir("" "")
   execute_process(COMMAND make -C ${tmp_dir} KERNEL_DIR=${KERNEL_DIR}
     WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
@@ -25,48 +25,30 @@ function(try_compile_prog_test)
   file(REMOVE_RECURSE ${CMAKE_CURRENT_BINARY_DIR}/${tmp_dir})
 endfunction()
 
-function(wait_for_pid pid)
-  execute_process(COMMAND ${CMAKE_SOURCE_DIR}/config/wait_for_pid.sh ${pid})
-endfunction()
-
-function(wait_for_pids)
-  # Wait for everyone to finish
-  foreach(pid ${pids})
-    wait_for_pid(${pid})
+function(run_conftests)
+  set(cmds_file "${CMAKE_CURRENT_BINARY_DIR}/conftest_cmds.txt")
+  file(WRITE ${cmds_file} "")
+  foreach(cmd ${conftest_cmds})
+    file(APPEND ${cmds_file} "${cmd}\n")
   endforeach()
+  execute_process(
+    COMMAND ${CMAKE_SOURCE_DIR}/config/run_parallel.sh ${cmds_file}
+    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR})
+  file(REMOVE ${cmds_file})
 endfunction()
 
-function(process_rl)
-  list(LENGTH pids list_len)
-  if(list_len EQUAL max_process)
-    list(GET pids 0 pid)
-    wait_for_pid(${pid})
-    list(REMOVE_AT pids 0)
-    set(pids "${pids}" CACHE INTERNAL "")
-  endif()
-endfunction()
+set(conftest_cmds "" CACHE INTERNAL "")
 
-# CMake "global" variable
-set(pids "" CACHE INTERNAL "")
-
-include(ProcessorCount)
-ProcessorCount(max_process)
-
-function(try_compile prologue body success_def fail_def)
-  # Rate limit processes
-  process_rl()
+function(add_compile_conftest prologue body success_def fail_def)
   set_conf_tmp_dir("${prologue}" "${body}")
-  execute_process(COMMAND ${CMAKE_SOURCE_DIR}/config/runbg.sh ${CMAKE_SOURCE_DIR}/config/compile_conftest.sh ${CMAKE_CURRENT_BINARY_DIR}/${tmp_dir} ${KERNEL_DIR} "${success_def}" "${fail_def}"
-    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-    OUTPUT_VARIABLE pid)
-  list(APPEND pids ${pid})
-  set(pids "${pids}" CACHE INTERNAL "")
+  set(cmd "${CMAKE_SOURCE_DIR}/config/compile_conftest.sh ${CMAKE_CURRENT_BINARY_DIR}/${tmp_dir} ${KERNEL_DIR} '${success_def}' '${fail_def}'")
+  list(APPEND conftest_cmds "${cmd}")
+  set(conftest_cmds "${conftest_cmds}" CACHE INTERNAL "")
 endfunction()
 
-function(try_compile_dev_or_ops fp_name prologue fn success_def fail_def)
+function(add_dev_op_compile_conftest fp_name prologue fn success_def fail_def)
   string(REGEX REPLACE ".*[ \t\r\n]+\\**([A-Za-z0-9_]+)[ \t\r\n]*\\(.*" "\\1" fn_name ${fn})
-  try_compile(
+  add_compile_conftest(
   "
 ${prologue}
 static ${fn}
@@ -78,7 +60,7 @@ struct ib_device_ops ops = {
   "
   "${success_def}" "${fail_def}")
 
-  try_compile(
+  add_compile_conftest(
   "
 ${prologue}
 static ${fn}
@@ -95,22 +77,22 @@ file(REMOVE ${CMAKE_CURRENT_BINARY_DIR}/config.h)
 file(REMOVE ${CMAKE_CURRENT_BINARY_DIR}/../output.log)
 
 message("-- Inspecting kernel")
-try_compile_prog_test()
+verify_kernel_headers()
 
-try_compile("#include <rdma/ib_umem.h>" "struct ib_umem_chunk chunk;" "" "HAVE_UMEM_SCATTERLIST_IF")
+add_compile_conftest("#include <rdma/ib_umem.h>" "struct ib_umem_chunk chunk;" "" "HAVE_UMEM_SCATTERLIST_IF")
 
-try_compile("" "struct ib_cq_init_attr attr;" HAVE_CREATE_CQ_ATTR "")
+add_compile_conftest("" "struct ib_cq_init_attr attr;" HAVE_CREATE_CQ_ATTR "")
 
-try_compile("" "struct rdma_ah_attr attr;" HAVE_CREATE_AH_RDMA_ATTR "")
+add_compile_conftest("" "struct rdma_ah_attr attr;" HAVE_CREATE_AH_RDMA_ATTR "")
 
-try_compile_dev_or_ops(post_send ""
+add_dev_op_compile_conftest(post_send ""
   "
 int efa_post_send(struct ib_qp *ibqp, const struct ib_send_wr *wr, const struct ib_send_wr **bad_wr)
   { return 0; }
   "
   HAVE_POST_CONST_WR "")
 
-try_compile(""
+add_compile_conftest(""
   "
 struct ib_device_attr attr = {
   .max_send_sge = 0,
@@ -119,29 +101,29 @@ struct ib_device_attr attr = {
   "
   HAVE_MAX_SEND_RCV_SGE "")
 
-try_compile("static int port_callback(struct ib_device *ibdev, u8 n, struct kobject *kobj) { return 0; }"
+add_compile_conftest("static int port_callback(struct ib_device *ibdev, u8 n, struct kobject *kobj) { return 0; }"
   "
 char name[2];
 ib_register_device(NULL, name, port_callback);
   "
   HAVE_IB_REGISTER_DEVICE_NAME_PARAM "")
 
-try_compile("" "ib_modify_qp_is_ok(0, 0, 0, 0);" HAVE_IB_MODIFY_QP_IS_OK_FOUR_PARAMS "")
+add_compile_conftest("" "ib_modify_qp_is_ok(0, 0, 0, 0);" HAVE_IB_MODIFY_QP_IS_OK_FOUR_PARAMS "")
 
-try_compile(""
+add_compile_conftest(""
   "
 pgprot_t prot;
 rdma_user_mmap_io(NULL, NULL, 0, 0, prot);
   "
   HAVE_RDMA_USER_MMAP_IO "")
 
-try_compile("" "struct ib_device_ops ops;" HAVE_IB_DEV_OPS "")
+add_compile_conftest("" "struct ib_device_ops ops;" HAVE_IB_DEV_OPS "")
 
-try_compile_dev_or_ops(create_ah ""
+add_dev_op_compile_conftest(create_ah ""
   "struct ib_ah *efa_kzalloc_ah(struct ib_pd *ibpd, struct rdma_ah_attr *ah_attr, u32 flags, struct ib_udata *udata) { return NULL; }"
   HAVE_CREATE_DESTROY_AH_FLAGS "")
 
-try_compile(
+add_compile_conftest(
   "
 #include <linux/scatterlist.h>
 #include <rdma/ib_umem.h>
@@ -155,7 +137,7 @@ for_each_sg_dma_page(umem->sg_head.sgl, sg_iter, umem->nmap, 0)
   "
   HAVE_SG_DMA_PAGE_ITER "")
 
-try_compile(""
+add_compile_conftest(""
   "
 struct ib_device_ops ops = {
   .size_ib_pd = 0,
@@ -163,7 +145,7 @@ struct ib_device_ops ops = {
   "
   HAVE_PD_CORE_ALLOCATION "")
 
-try_compile(""
+add_compile_conftest(""
   "
 struct ib_device_ops ops = {
   .size_ib_ucontext = 0,
@@ -171,7 +153,7 @@ struct ib_device_ops ops = {
   "
   HAVE_UCONTEXT_CORE_ALLOCATION "")
 
-try_compile(""
+add_compile_conftest(""
   "
 struct ib_device dev = {
   .kverbs_provider = 0,
@@ -179,14 +161,14 @@ struct ib_device dev = {
   "
   HAVE_NO_KVERBS_DRIVERS "")
 
-try_compile("#include <rdma/ib_umem.h>"
+add_compile_conftest("#include <rdma/ib_umem.h>"
   "
 struct ib_udata *udata;
 ib_umem_get(udata, 0, 0, 0, 0);
   "
   HAVE_IB_UMEM_GET_UDATA "")
 
-try_compile(
+add_compile_conftest(
   "
 #include <rdma/uverbs_ioctl.h>
 
@@ -200,7 +182,7 @@ rdma_udata_to_drv_context(udata, struct efa_ucontext, ibucontext);
   "
   HAVE_UDATA_TO_DRV_CONTEXT "")
 
-try_compile(""
+add_compile_conftest(""
   "
 struct ib_device *dev;
 char name[2];
@@ -208,7 +190,7 @@ ib_register_device(dev, name);
   "
   HAVE_IB_REGISTER_DEVICE_TWO_PARAMS "")
 
-try_compile(
+add_compile_conftest(
   "
 struct efa_dev {
   struct ib_device ibdev;
@@ -216,7 +198,7 @@ struct efa_dev {
   "
   "ib_alloc_device(efa_dev, ibdev);" HAVE_SAFE_IB_ALLOC_DEVICE "")
 
-try_compile(""
+add_compile_conftest(""
   "
 struct ib_device_ops ops = {
   .size_ib_ah = 0,
@@ -224,35 +206,35 @@ struct ib_device_ops ops = {
   "
   HAVE_AH_CORE_ALLOCATION "")
 
-try_compile_dev_or_ops(alloc_pd ""
+add_dev_op_compile_conftest(alloc_pd ""
   "int efa_alloc_pd(struct ib_pd *ibpd, struct ib_udata *udata) { return 0; }"
   HAVE_ALLOC_PD_NO_UCONTEXT "")
 
-try_compile_dev_or_ops(create_cq ""
+add_dev_op_compile_conftest(create_cq ""
   "struct ib_cq *efa_kzalloc_cq(struct ib_device *ibdev, const struct ib_cq_init_attr *attr, struct ib_udata *udata) { return NULL; }"
   HAVE_CREATE_CQ_NO_UCONTEXT "")
 
-try_compile_dev_or_ops(dealloc_pd ""
+add_dev_op_compile_conftest(dealloc_pd ""
   "void efa_dealloc_pd(struct ib_pd *ibpd, struct ib_udata *udata) {}"
   HAVE_DEALLOC_PD_UDATA "")
 
-try_compile_dev_or_ops(dereg_mr ""
+add_dev_op_compile_conftest(dereg_mr ""
   "int efa_dereg_mr(struct ib_mr *ibmr, struct ib_udata *udata) { return 0; }"
   HAVE_DEREG_MR_UDATA "")
 
-try_compile_dev_or_ops(destroy_qp ""
+add_dev_op_compile_conftest(destroy_qp ""
   "int efa_destroy_qp(struct ib_qp *ibqp, struct ib_udata *udata) { return 0; }"
   HAVE_DESTROY_QP_UDATA "")
 
 # This check includes: struct ib_block_iter, __rdma_block_iter_start, __rdma_block_iter_next,
 # rdma_block_iter_dma_address, rdma_for_each_block, ib_umem_find_best_pgsz
-try_compile("#include <rdma/ib_umem.h>" "ib_umem_find_best_pgsz(NULL, 0, 0);"
+add_compile_conftest("#include <rdma/ib_umem.h>" "ib_umem_find_best_pgsz(NULL, 0, 0);"
   HAVE_IB_UMEM_FIND_SINGLE_PG_SIZE "")
 
-try_compile("" "enum rdma_driver_id driver_id = RDMA_DRIVER_EFA;"
+add_compile_conftest("" "enum rdma_driver_id driver_id = RDMA_DRIVER_EFA;"
   HAVE_UPSTREAM_EFA "")
 
-try_compile(""
+add_compile_conftest(""
   "
 struct ib_device_ops ops = {
   .driver_id = 0,
@@ -260,11 +242,11 @@ struct ib_device_ops ops = {
   "
   HAVE_IB_DEVICE_OPS_COMMON "")
 
-try_compile_dev_or_ops(destroy_cq ""
+add_dev_op_compile_conftest(destroy_cq ""
   "void efa_destroy_cq(struct ib_cq *ibcq, struct ib_udata *udata) {}"
   HAVE_IB_VOID_DESTROY_CQ_UDATA "")
 
-try_compile(""
+add_compile_conftest(""
   "
 struct ib_device_ops ops = {
   .size_ib_cq = 0,
@@ -272,7 +254,7 @@ struct ib_device_ops ops = {
   "
   HAVE_CQ_CORE_ALLOCATION "")
 
-try_compile(""
+add_compile_conftest(""
   "
 struct ib_device dev = {
   .driver_def = 0,
@@ -280,7 +262,7 @@ struct ib_device dev = {
   "
   HAVE_IB_DEVICE_DRIVER_DEF "")
 
-try_compile(
+add_compile_conftest(
   "
 #include <rdma/uverbs_ioctl.h>"
   "
@@ -290,27 +272,27 @@ struct uverbs_attr_bundle bundle = {
   "
   HAVE_ATTR_BUNDLE_DRIVER_UDATA "")
 
-try_compile(""
+add_compile_conftest(""
   "int a = IB_PORT_PHYS_STATE_LINK_UP;"
   HAVE_IB_PORT_PHYS_STATE_LINK_UP "")
 
-try_compile(""
+add_compile_conftest(""
   "
 struct ib_device *dev;
 ibdev_err_ratelimited(dev, \"Hello\");
   "
   HAVE_IBDEV_PRINT_RATELIMITED "")
 
-try_compile(""
+add_compile_conftest(""
   "
 struct ib_device *dev;
 ibdev_err(dev, \"World\");
   "
   HAVE_IBDEV_PRINT "")
 
-try_compile("" "int a = IB_QPT_DRIVER;" HAVE_IB_QPT_DRIVER "")
+add_compile_conftest("" "int a = IB_QPT_DRIVER;" HAVE_IB_QPT_DRIVER "")
 
-try_compile(""
+add_compile_conftest(""
   "
 struct ib_device dev = {
   .driver_id = 0,
@@ -318,7 +300,7 @@ struct ib_device dev = {
   "
   HAVE_DRIVER_ID "")
 
-try_compile(""
+add_compile_conftest(""
   "
 struct ib_mr mr = {
   .type = 0,
@@ -326,49 +308,49 @@ struct ib_mr mr = {
   "
   HAVE_IB_MR_TYPE "")
 
-try_compile("#include <linux/pci_ids.h>" "int a = PCI_VENDOR_ID_AMAZON;"
+add_compile_conftest("#include <linux/pci_ids.h>" "int a = PCI_VENDOR_ID_AMAZON;"
   HAVE_PCI_VENDOR_ID_AMAZON "")
 
-try_compile("#include <rdma/ib_umem.h>" "ib_umem_get(NULL, 0, 0, 0);"
+add_compile_conftest("#include <rdma/ib_umem.h>" "ib_umem_get(NULL, 0, 0, 0);"
   HAVE_IB_UMEM_GET_NO_DMASYNC "")
 
-try_compile("#include <rdma/ib_umem.h>"
+add_compile_conftest("#include <rdma/ib_umem.h>"
   "
 pgprot_t pgt;
 rdma_user_mmap_io(NULL, NULL, 0, 0, pgt, NULL);
   "
   HAVE_CORE_MMAP_XA "")
 
-try_compile("" "int a = RDMA_NODE_UNSPECIFIED;" HAVE_RDMA_NODE_UNSPECIFIED "")
+add_compile_conftest("" "int a = RDMA_NODE_UNSPECIFIED;" HAVE_RDMA_NODE_UNSPECIFIED "")
 
-try_compile("#include <rdma/ib_umem.h>"
+add_compile_conftest("#include <rdma/ib_umem.h>"
   "
 struct ib_device *dev;
 ib_umem_get(dev, 0, 0, 0);
   "
   HAVE_IB_UMEM_GET_DEVICE_PARAM "")
 
-try_compile("" "int a = IB_ACCESS_OPTIONAL;" HAVE_IB_ACCESS_OPTIONAL "")
+add_compile_conftest("" "int a = IB_ACCESS_OPTIONAL;" HAVE_IB_ACCESS_OPTIONAL "")
 
-try_compile("" "struct rdma_ah_init_attr ah_attr;" HAVE_CREATE_AH_INIT_ATTR "")
+add_compile_conftest("" "struct rdma_ah_init_attr ah_attr;" HAVE_CREATE_AH_INIT_ATTR "")
 
-try_compile_dev_or_ops(alloc_mr ""
+add_dev_op_compile_conftest(alloc_mr ""
   "struct ib_mr *efa_alloc_mr(struct ib_pd *pd, enum ib_mr_type mr_type, u32 max_num_sg, struct ib_udata *udata) { return NULL; }"
   HAVE_ALLOC_MR_UDATA "")
 
-try_compile_dev_or_ops(dealloc_pd ""
+add_dev_op_compile_conftest(dealloc_pd ""
   "int efa_dealloc_pd(struct ib_pd *ibpd, struct ib_udata *udata) { return 0; }"
   HAVE_DEALLOC_PD_UDATA_RC "")
 
-try_compile_dev_or_ops(destroy_ah ""
+add_dev_op_compile_conftest(destroy_ah ""
   "int efa_destroy_ah(struct ib_ah *ibah, u32 flags) { return 0; }"
   HAVE_AH_CORE_ALLOCATION_DESTROY_RC "")
 
-try_compile_dev_or_ops(destroy_cq ""
+add_dev_op_compile_conftest(destroy_cq ""
   "int efa_destroy_cq(struct ib_cq *ibcq, struct ib_udata *udata) { return 0; }"
   HAVE_IB_INT_DESTROY_CQ_UDATA "")
 
-try_compile("#include <rdma/ib_umem.h>"
+add_compile_conftest("#include <rdma/ib_umem.h>"
   "
 struct ib_block_iter biter;
 struct ib_umem *umem;
@@ -379,7 +361,7 @@ rdma_umem_for_each_dma_block(umem, &biter, 0)
   "
   HAVE_RDMA_UMEM_FOR_EACH_DMA_BLOCK "")
 
-try_compile("#include <rdma/ib_umem.h>"
+add_compile_conftest("#include <rdma/ib_umem.h>"
   "
 struct ib_umem *umem;
 
@@ -387,7 +369,7 @@ ib_umem_num_dma_blocks(umem, 0);
   "
   HAVE_IB_UMEM_NUM_DMA_BLOCKS "")
 
-try_compile(""
+add_compile_conftest(""
   "
 struct ib_device *dev;
 char name[2];
@@ -396,27 +378,27 @@ ib_register_device(dev, name, dma_device);
   "
   HAVE_IB_REGISTER_DEVICE_DMA_DEVICE_PARAM "")
 
-try_compile_dev_or_ops(create_user_ah ""
+add_dev_op_compile_conftest(create_user_ah ""
   "int efa_create_ah(struct ib_ah *ibah, struct rdma_ah_init_attr *init_attr, struct ib_udata *udata) { return 0; }"
   HAVE_UVERBS_CMD_MASK_NOT_NEEDED "")
 
-try_compile_dev_or_ops(query_port ""
+add_dev_op_compile_conftest(query_port ""
   "int efa_query_port(struct ib_device *ibdev, u32 port, struct ib_port_attr *props) { return 0; }"
   HAVE_U32_PORT "")
 
-try_compile_dev_or_ops(alloc_hw_port_stats ""
+add_dev_op_compile_conftest(alloc_hw_port_stats ""
   "struct rdma_hw_stats *efa_alloc_hw_port_stats(struct ib_device *ibdev, u32 port_num) { return 0; }"
   HAVE_SPLIT_STATS_ALLOC "")
 
-try_compile("#include <linux/sysfs.h>"
+add_compile_conftest("#include <linux/sysfs.h>"
   "
 sysfs_emit(NULL, \"Test\");
   "
   HAVE_SYSFS_EMIT "")
 
-try_compile("#include <linux/xarray.h>" "xa_load(NULL, 0);" HAVE_XARRAY "")
+add_compile_conftest("#include <linux/xarray.h>" "xa_load(NULL, 0);" HAVE_XARRAY "")
 
-try_compile(""
+add_compile_conftest(""
   "
 struct ib_device_ops ops = {
   .size_ib_qp = 0,
@@ -424,10 +406,10 @@ struct ib_device_ops ops = {
   "
   HAVE_QP_CORE_ALLOCATION "")
 
-try_compile("" "struct rdma_stat_desc desc;" HAVE_STAT_DESC_STRUCT "")
+add_compile_conftest("" "struct rdma_stat_desc desc;" HAVE_STAT_DESC_STRUCT "")
 
 # dynamic DMA-buf handling and ibcore device method are required.
-try_compile(
+add_compile_conftest(
   "
 #include <linux/dma-resv.h>
 #include <linux/dma-buf.h>
@@ -440,30 +422,30 @@ struct ib_device_ops ops = {
   "
   HAVE_MR_DMABUF "")
 
-try_compile_dev_or_ops(reg_user_mr_dmabuf ""
+add_dev_op_compile_conftest(reg_user_mr_dmabuf ""
   "
 struct ib_mr *efa_reg_user_mr_dmabuf(struct ib_pd *ibpd, u64 start, u64 length, u64 virt_addr,
                                      int fd, int access_flags, struct uverbs_attr_bundle *attrs) { return 0; }
   "
   HAVE_REG_USER_MR_DMABUF_BUNDLE "")
 
-try_compile("#include <rdma/ib_umem.h>" "ib_umem_dmabuf_get_pinned(NULL, 0, 0, 0, 0);" HAVE_IB_UMEM_DMABUF_PINNED "")
+add_compile_conftest("#include <rdma/ib_umem.h>" "ib_umem_dmabuf_get_pinned(NULL, 0, 0, 0, 0);" HAVE_IB_UMEM_DMABUF_PINNED "")
 
-try_compile("#include <linux/module.h>" "MODULE_IMPORT_NS(TEST);" HAVE_MODULE_IMPORT_NS "")
+add_compile_conftest("#include <linux/module.h>" "MODULE_IMPORT_NS(TEST);" HAVE_MODULE_IMPORT_NS "")
 
-try_compile_dev_or_ops(create_cq ""
+add_dev_op_compile_conftest(create_cq ""
   "int efa_create_cq(struct ib_cq *ibcq, const struct ib_cq_init_attr *attr, struct uverbs_attr_bundle *attrs) { return 0; }"
   HAVE_CREATE_CQ_BUNDLE "")
 
-try_compile_dev_or_ops(create_cq_umem ""
+add_dev_op_compile_conftest(create_cq_umem ""
 "int efa_create_cq(struct ib_cq *ibcq, const struct ib_cq_init_attr *attr, struct ib_umem *umem, struct uverbs_attr_bundle *attrs) { return 0; }"
   HAVE_CREATE_CQ_UMEM "")
 
-try_compile("" "fallthrough;" HAVE_FALLTHROUGH "")
+add_compile_conftest("" "fallthrough;" HAVE_FALLTHROUGH "")
 
-try_compile("#include <linux/log2.h>" "u64 x = bits_per(1);" HAVE_LOG2_BITS_PER "")
+add_compile_conftest("#include <linux/log2.h>" "u64 x = bits_per(1);" HAVE_LOG2_BITS_PER "")
 
-try_compile(
+add_compile_conftest(
   "
 #include <rdma/ib_umem.h>
   "
@@ -473,7 +455,7 @@ struct sg_append_table sgt = umem->sgt_append;
   "
   HAVE_IB_UMEM_SGT_APPEND "")
 
-try_compile(
+add_compile_conftest(
   "
 #include <rdma/uverbs_ioctl.h>
   "
@@ -483,7 +465,7 @@ attrs = rdma_udata_to_uverbs_attr_bundle(NULL);
   "
   HAVE_UDATA_TO_UVERBS_ATTR_BUNDLE "")
 
-try_compile(
+add_compile_conftest(
   "
 #include <rdma/uverbs_ioctl.h>
   "
@@ -493,7 +475,7 @@ fd = uverbs_get_raw_fd(NULL, NULL, 0);
   "
   HAVE_UVERBS_ATTR_RAW_FD "")
 
-try_compile(
+add_compile_conftest(
   "
 #include <rdma/ib_umem.h>
   "
@@ -505,7 +487,7 @@ addr = ib_umem_start_dma_addr(umem);
   "
   HAVE_IB_UMEM_START_DMA_ADDR "")
 
-try_compile(
+add_compile_conftest(
   "
 #include <rdma/ib_umem.h>
   "
@@ -517,7 +499,7 @@ contiguous = ib_umem_is_contiguous(umem);
   "
   HAVE_IB_UMEM_IS_CONTIGUOUS "")
 
-try_compile_dev_or_ops(reg_user_mr_dmabuf ""
+add_dev_op_compile_conftest(reg_user_mr_dmabuf ""
   "
 struct ib_mr *efa_reg_user_mr_dmabuf(struct ib_pd *ibpd, u64 start, u64 length, u64 virt_addr,
                                      int fd, int access_flags, struct ib_dmah *dmah,
@@ -525,7 +507,7 @@ struct ib_mr *efa_reg_user_mr_dmabuf(struct ib_pd *ibpd, u64 start, u64 length, 
   "
   HAVE_REG_USER_MR_DMAH "")
 
-try_compile(
+add_compile_conftest(
   "
 #include <errname.h>
   "
@@ -534,5 +516,5 @@ char *name = errname(ERR_PTR(-EIO));
   "
   HAVE_PRINT_ERR_PTR "")
 
-wait_for_pids()
+run_conftests()
 message("-- Inspecting kernel - done")

kernel/linux/efa/config/runbg.sh
@@ -1,6 +1,0 @@
-#!/bin/bash
-# SPDX-License-Identifier: GPL-2.0 OR BSD-2-Clause
-# Copyright 2021 Amazon.com, Inc. or its affiliates. All rights reserved.
-
-nohup "$@" &>/dev/null </dev/null &
-echo $!

kernel/linux/efa/config/wait_for_pid.sh
@@ -1,19 +1,0 @@
-#!/bin/bash
-# SPDX-License-Identifier: GPL-2.0 OR BSD-2-Clause
-# Copyright 2021 Amazon.com, Inc. or its affiliates. All rights reserved.
-
-pid=$1
-
-while true; do
-	ls /proc/$pid &>/dev/null
-	err=$?
-	if [[ $err -ne 0 ]]; then
-		break
-	else
-		# Docker doesn't reap zombies by default, handle them explicitly
-		if [[ $(cat /proc/$pid/stat 2>/dev/null | awk '{ print $3 }') == "Z" ]]; then
-			break
-		fi
-	fi
-	sleep 0.2
-done

kernel/linux/efa/debian/rules
@@ -69,6 +69,5 @@ override_dh_auto_install:
 	install -m 644 config/Makefile		${DESTDIR}/${INSTALL_PATH}/config
 	install -m 644 config/main.c.in		${DESTDIR}/${INSTALL_PATH}/config
 	install -m 744 config/compile_conftest.sh	${DESTDIR}/${INSTALL_PATH}/config
-	install -m 744 config/wait_for_pid.sh	${DESTDIR}/${INSTALL_PATH}/config
+	install -m 744 config/run_parallel.sh	${DESTDIR}/${INSTALL_PATH}/config
 	install -m 644 config/efa.cmake		${DESTDIR}/${INSTALL_PATH}/config
-	install -m 744 config/runbg.sh		${DESTDIR}/${INSTALL_PATH}/config

kernel/linux/efa/rpm/efa.spec
@@ -71,9 +71,8 @@ install -m 644 RELEASENOTES.md		%{buildroot}%{install_path}
 install -m 644 config/Makefile		%{buildroot}%{install_path}/config
 install -m 644 config/main.c.in		%{buildroot}%{install_path}/config
 install -m 744 config/compile_conftest.sh	%{buildroot}%{install_path}/config
+install -m 744 config/run_parallel.sh	%{buildroot}%{install_path}/config
 install -m 644 config/efa.cmake		%{buildroot}%{install_path}/config
-install -m 744 config/runbg.sh		%{buildroot}%{install_path}/config
-install -m 744 config/wait_for_pid.sh	%{buildroot}%{install_path}/config
 cd src
 install -m 644 efa_com.c		%{buildroot}%{install_path}/src
 install -m 644 efa_com_cmd.c		%{buildroot}%{install_path}/src

commit f9a4d7de897d0f5774625c413cbe3a580650faa4

linux/efa: Limit number of conftests running in parallel
Collecting all conftests into a single batch removed the rate limiter
that previously kept at most one compile per processor in flight. The
build now starts every conftest at once, which can exhaust the memory.
Compilation processes then get killed, and since a conftest that dies
is indistinguishable from one reporting a missing kernel feature, the
build can silently configure itself wrong.

Restore the limit, at half the processors. Every conftest stays a
direct child of this shell, so the premature return that motivated the
single runner cannot come back.

Fixes: 4940b5a3e673 ("linux/efa: Replace conftest PID wait with single parallel runner")
Reviewed-by: Firas Jahjah <firasj@amazon.com>
Reviewed-by: Yonatan Nachum <ynachum@amazon.com>
Signed-off-by: Dan Cherniavsky <dchern@amazon.com>
Signed-off-by: Michael Margolin <mrgolin@amazon.com>

kernel/linux/efa/config/run_parallel.sh
@@ -4,8 +4,18 @@
 
 # Execute commands from a file in parallel and wait for all to complete.
 
+nproc=$(nproc 2>/dev/null)
+[[ $nproc =~ ^[1-9][0-9]*$ ]] || nproc=4
+max_jobs=$(( nproc / 2 > 0 ? nproc / 2 : 1 ))
+
+pids=()
 while IFS= read -r cmd; do
 	eval "$cmd" &
+	pids+=($!)
+	if [[ ${#pids[@]} -ge $max_jobs ]]; then
+		wait "${pids[0]}" 2>/dev/null
+		pids=("${pids[@]:1}")
+	fi
 done < "$1"
 
 wait